0e57528e8e78367fd5838d6d8786a7ad287c2e17
ReubenGVarghese <ReubenGVarghese@gmail.com>
2025-11-28 14:52:50 -0500

    Vercel Adaptation and backend api calls seperate from frontend
    Fixed frontend and backend and modified to be deployable by vercel

--- Playlist-Maker/vite.config.js
+++ Playlist-Maker/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    // ADD THIS LINE:
-    host: '127.0.0.1', 
-    // Keep existing settings
-    port: 8888,
-    strictPort: true,
-    open: true, // This tells Vite to open the browser automatically
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
   }
 })
